6bf5b31519131127303561a6d90f09b721aa1ba4
Guilherme Oliveira <guilhergoa@hotmail.com>
2022-09-13 21:09:13 -0300

    :bug: fix: remove unused imports and variables

--- Codigo/src/extension-configuration/core/extension-configuration-service.ts
+++ Codigo/src/extension-configuration/core/extension-configuration-service.ts
@@ -1,15 +1,12 @@
 import { injectable } from "inversify";
 import { Observable, ReplaySubject, startWith } from "rxjs";
 import { window } from "vscode";
-import { appInjector } from "../../inversify.config";
-import { GitService } from "../../version-control/core/git-service";
 import { ConfigurationData } from "../models/configuration-data";
 
 @injectable()
 export class ExtensionConfigurationService {
   private configurationData = new ReplaySubject<ConfigurationData>();
   private actualState = new ConfigurationData(true, false, "", false);
-  private gitService = appInjector.get(GitService);
 
   public getConfigurationData(): Observable<ConfigurationData> {
     return this.configurationData.pipe(startWith(this.actualState));
