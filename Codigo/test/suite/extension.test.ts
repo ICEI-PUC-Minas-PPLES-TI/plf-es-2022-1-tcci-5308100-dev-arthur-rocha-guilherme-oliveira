54f0c8106d55ed7882871c180cc5586d9dc68d90
Arthur Amaral <arthurramaral@outlook.com>
2022-10-28 00:32:14 -0300

    testing 2

--- Codigo/test/suite/extension.test.ts
+++ Codigo/test/suite/extension.test.ts
@@ -4,23 +4,17 @@ import * as assert from "assert";
 // as well as import your extension to test it
 import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
-import { ActivityBar } from "vscode-extension-tester";
-import { expect } from "chai";
+import * as extest from "vscode-extension-tester";
+import * as chai from "chai";
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
-  let activityBar: ActivityBar;
 
-  before(async () => {
-    // init the activity bar page object
-    activityBar = new ActivityBar();
-  });
-
-  // Test what view controls are available
-  it("Shows explorer view control (container)", async () => {
+  test("Shows explorer view control (container)", async (doneFn) => {
+    const activityBar = new extest.ActivityBar();
     // get all the view controls
     const controls = await activityBar.getViewControls();
-    expect(controls).not.empty;
+    chai.expect(controls).not.empty;
 
     // get titles from the controls
     const titles = await Promise.all(
@@ -31,11 +25,11 @@ suite("Extension Test Suite", () => {
 
     // assert a view control named 'Explorer' is present
     // the keyboard shortcut is part of the title, so we do a little transformation
-    expect(titles.some((title) => title.startsWith("Explorer"))).is.true;
-  });
+    chai.expect(titles.some((title) => title.startsWith("Explorer"))).is.true;
 
-  test("Sample test", () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+
+    doneFn();
   });
 });
